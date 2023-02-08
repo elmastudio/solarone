5e2144317c2c4cf3ff11b760eb21852caa5d0ddc
Ellen Bauer <ellen.bauer@elmastudio.de>
2023-02-08 21:10:28 +1300

    Update functions.php

--- functions.php
+++ functions.php
@@ -118,47 +118,6 @@ add_action( 'init', 'solarone_register_pattern_categories' );
  */
 require get_template_directory() . '/inc/block-styles.php';
 
-/**
- * TGMPA plugin activation.
- */
-require_once get_template_directory() . '/inc/classes/class-tgm-plugin-activation.php';
-
-add_action( 'tgmpa_register', 'solarone_register_required_plugins' );
-
-/**
- * Register the required plugins for this theme.
- */
-function solarone_register_required_plugins() {
-
-	/*
-	 * Array of plugin arrays. Required keys are name and slug.
-	 */
-	$plugins = array(
-
-		array(
-			'name'      => 'AinoBlocks - Gutenberg Website Builder Blocks',
-			'slug'      => 'aino-blocks',
-			'required'  => false,
-		),
-	);
-
-	/*
-	 * Array of configuration settings. Amend each line as needed.
-	 */
-	$config = array(
-		'id'           => 'solarone',                 // Unique ID for hashing notices for multiple instances of TGMPA.
-		'default_path' => '',                      // Default absolute path to bundled plugins.
-		'menu'         => 'tgmpa-install-plugins', // Menu slug.
-		'has_notices'  => true,                    // Show admin notices or not.
-		'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
-		'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
-		'is_automatic' => false,                   // Automatically activate plugins after installation or not.
-		'message'      => '',                      // Message to output right before the plugins table.
-
-	);
-
-	tgmpa( $plugins, $config );
-}
 
 /**
 * Theme Setup Wizard.
